e1879975e9301852434a8f5a3a87428626cfbdb8
SohamBhandary <bhandarysoham27@gmail.com>
2025-04-04 17:14:27 +0530

    DI using spring boot

---
+++ src/main/java/com/example/Spring/Aliens.java
@@ -0,0 +1,10 @@
+package com.example.Spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Aliens {
+    public void code(){
+        System.out.println("coding");
+    }
+}
---
+++ src/main/java/com/example/Spring/Application.java
@@ -0,0 +1,20 @@
+package com.example.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+	 ApplicationContext context= SpringApplication.run(Application.class, args);
+	Aliens a =context.getBean(Aliens.class);
+	a.code();
+		Aliens a1 =context.getBean(Aliens.class);
+		a1.code();
+
+
+	}
+
+}
